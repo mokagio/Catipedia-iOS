236c868f2bcacba0d100f8be534c77c8a0fe4aeb
Gio <giovanni.lodi42@gmail.com>
2013-06-06 09:55:24 +0300

    Added better feedback messages

--- Catipedia/MRViewController.m
+++ Catipedia/MRViewController.m
@@ -26,6 +26,9 @@ static const CGFloat kToastMessageInterval = 1.0;
 - (void)addTakePictureButton;
 - (void)loadTakePictureController;
 - (void)uploadPictureFromPath:(NSString *)picturePath;
+- (UIImage *)treatedImage:(UIImage *)originalImage;
+- (void)showSuccessToast;
+- (void)showFailToast;
 - (void)removeToastMessage;
 @end
 
@@ -47,7 +50,9 @@ static const CGFloat kToastMessageInterval = 1.0;
 {
     self.takePictureButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
     self.takePictureButton.frame = CGRectMake(0, 0, 160, 80);
-    self.takePictureButton.center = self.view.center;
+    CGPoint center = self.view.center;
+    center.y += 160;
+    self.takePictureButton.center = center;
     [self.takePictureButton setTitle:@"Take Picture" forState:UIControlStateNormal];
     [self.takePictureButton addTarget:self
                                action:@selector(loadTakePictureController)
@@ -83,22 +88,31 @@ static const CGFloat kToastMessageInterval = 1.0;
                           progress:^(NSUInteger bytesWritten, long long totalBytesWritten, long long totalBytesExpectedToWrite) {
                               NSLog(@"%f%% Uploaded", (totalBytesWritten / (totalBytesExpectedToWrite * 1.0f) * 100));
                           } success:^(id responseObject) {
-                              [self dismissViewControllerAnimated:YES completion:^{
-                                  NSLog(@"Upload Complete");
-                              }];
+                              [MBProgressHUD hideAllHUDsForView:self.view animated:YES];
+                              [self showSuccessToast];
+                              NSLog(@"Upload Complete");
                           } failure:^(NSError *error) {
-                              [self dismissViewControllerAnimated:YES completion:^{
-                                  NSLog(@"Error: %@", error);
-                              }];
+                              [MBProgressHUD hideAllHUDsForView:self.view animated:YES];
+                              [self showFailToast];
+                              NSLog(@"Error: %@", error);
                           }];
 }
 
-- (void)showFeedbackToast
+- (void)showSuccessToast
+{
+    [self showToastWithMessage:@"Done"];
+}
+
+- (void)showFailToast
+{
+    [self showToastWithMessage:@"Failed"];
+}
+
+- (void)showToastWithMessage:(NSString *)message
 {
     self.toastMessage = [MBProgressHUD showHUDAddedTo:self.view animated:YES];
     self.toastMessage.mode = MBProgressHUDModeText;
-    self.toastMessage.labelText = @"Picture Saved";
-    self.toastMessage.yOffset = 100;
+    self.toastMessage.labelText = message;
     
     [self performSelector:@selector(removeToastMessage) withObject:nil afterDelay:kToastMessageInterval];
 }
@@ -108,6 +122,20 @@ static const CGFloat kToastMessageInterval = 1.0;
     [MBProgressHUD hideAllHUDsForView:self.view animated:YES];
 }
 
+- (UIImage *)treatedImage:(UIImage *)originalImage
+{
+    UIImage *resizedImage = [originalImage copy];
+    CGFloat scaleFactor = 0.25;
+    CGFloat resizedHeight = resizedImage.size.height * scaleFactor;
+    CGFloat resizedWidth = resizedImage.size.width * scaleFactor;
+    CGSize resizedSize = CGSizeMake(resizedWidth, resizedHeight);
+    UIGraphicsBeginImageContextWithOptions(resizedSize, NO, 0.0f);
+    [resizedImage drawInRect:CGRectMake(0, 0, resizedSize.width, resizedSize.height)];
+    resizedImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return resizedImage;
+}
+
 @end
 
 @implementation MRViewController (CameraDelegate)
@@ -115,32 +143,31 @@ static const CGFloat kToastMessageInterval = 1.0;
 - (void)imagePickerController:(UIImagePickerController *)picker
 didFinishPickingMediaWithInfo:(NSDictionary *)info {
     
+    [picker dismissViewControllerAnimated:YES completion:nil];
+    
     NSString *mediaType = [info objectForKey:UIImagePickerControllerMediaType];
     if (CFStringCompare((CFStringRef)mediaType, kUTTypeImage, 0) == kCFCompareEqualTo) {
         UIImage *imageToSave = (UIImage *)[info objectForKey:UIImagePickerControllerOriginalImage];
         
         // Save image to library
+        MBProgressHUD *progress = [MBProgressHUD showHUDAddedTo:self.view animated:YES];
+        progress.mode = MBProgressHUDModeIndeterminate;
+        progress.labelText = @"uploading";
+        
         ALAssetsLibrary *library = [[ALAssetsLibrary alloc] init];
         [library writeImageToSavedPhotosAlbum:[imageToSave CGImage]
                                   orientation:(ALAssetOrientation)[imageToSave imageOrientation]
                               completionBlock:^(NSURL *assetURL, NSError *error){
                                   if (error) {
-                                      NSLog(@"error");
-                                      [picker dismissViewControllerAnimated:YES completion:nil];
+                                      [MBProgressHUD hideAllHUDsForView:self.view animated:YES];
+                                      [self showFailToast];
+                                      NSLog(@"There was an error saving to camera roll");
                                   } else {
                                       // Save image to temp file beacuse damned iOS doesn't allow us to access the Camera Roll
                                       // (?)
-                                      UIImage *resizedImage = [imageToSave copy];
-                                      CGFloat scaleFactor = 0.25;
-                                      CGFloat resizedHeight = resizedImage.size.height * scaleFactor;
-                                      CGFloat resizedWidth = resizedImage.size.width * scaleFactor;
-                                      CGSize resizedSize = CGSizeMake(resizedWidth, resizedHeight);
-                                      UIGraphicsBeginImageContextWithOptions(resizedSize, NO, 0.0f);
-                                      [resizedImage drawInRect:CGRectMake(0, 0, resizedSize.width, resizedSize.height)];
-                                      resizedImage = UIGraphicsGetImageFromCurrentImageContext();
-                                      UIGraphicsEndImageContext();
+                                      UIImage *resizedImage = [self treatedImage:imageToSave];
                                       
-                                      NSData *data = UIImageJPEGRepresentation(resizedImage, 0.5);
+                                      NSData *data = UIImageJPEGRepresentation(resizedImage, 0.25);
                                       NSString *fileName = [NSString stringWithFormat:@"temp-%f.jpg", [[NSDate date] timeIntervalSince1970]];
                                       NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
                                       NSString *documentsDirectory = [paths objectAtIndex:0];
@@ -149,9 +176,9 @@ didFinishPickingMediaWithInfo:(NSDictionary *)info {
                                       if (success) {
                                           [self uploadPictureFromPath:path];
                                       } else {
-                                          [picker dismissViewControllerAnimated:YES completion:^{
-                                              NSLog(@"There was an error");
-                                          }];
+                                          [MBProgressHUD hideAllHUDsForView:self.view animated:YES];
+                                          [self showFailToast];
+                                          NSLog(@"There was an error saving the temp file");
                                       }
                                   }
                               }];
